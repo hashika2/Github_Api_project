24aa91149f9b6c3c89dafde6bef09294c35ec64d
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-01 19:42:39 +0530

    profile
    set profile width

--- ui.js
+++ ui.js
@@ -9,8 +9,8 @@ class UI {
       <div class="card card-body mb-3">
         <div class="row">
           <div class="col-md-3">
-            <img class="img-fluid mb-2" src="${user.avatar_url}" width="270px">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            <img class="img-fluid " src="${user.avatar_url}" width="270px" >
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-5">View Profile</a>
           </div>
           <div class="col-md-9">
             <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
