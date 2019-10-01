727df682c543e24acc87e93843cc95f960787f6b
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-01 15:05:42 +0530

    set ui
    set user interface for showing github users

--- ui.js
+++ ui.js
@@ -4,6 +4,33 @@ class UI{
     }
 
     showProfile(user){
-        console.log(user);
+        this.profile.innerHTML=`
+        <div class="card card-body mb-3>
+            <div class="row">
+                <div class="col-md-3">
+                <img class="img-fluid mb-2" src="${user.avatar_url}">
+                <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                </div>
+                <div class="col-md-9">
+                <span class="badge badge-primary">Public Repose: ${user.public_repos}</span>
+                <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                <span class="badge badge-success">Folowers: ${user.followers}</span>
+                <span class="badge badge-info">Following: ${user.following}</span>
+                <br><br>
+                <ui class="list-group">
+                <li class="list-group-item">Company:${user.company}</li>
+                <li class="list-group-item">Website/Blog:${user.blog}</li>
+                <li class="list-group-item">Location:${user.location}</li>
+                <li class="list-group-item">Member Since:${user.created_at}</li>
+                </ui>
+              
+                </div>
+                </div>
+            </div>
+        </div> 
+        <h3 class="page-heading mb-3">Latest Repos</h3>'
+        <div id="repos"></div>`
+         
+        ;
     }
 }
