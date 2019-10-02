180418966434afb03154abdc3c55b58766f23d4f
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-02 14:05:01 +0530

    set profile
    set diplay the repository hwich you are doing these days

--- github.js
+++ github.js
@@ -1,17 +1,22 @@
-class Github{
-    constructor(){
-        this.client_id='a760ba8d4f0f7441a01c';
-        this.client_secret='0b9670dcce334f380b4f0ebfa7f37330b69ce8d4';
+class Github {
+  constructor() {
+    this.client_id='a760ba8d4f0f7441a01c';
+    this.client_secret='0b9670dcce334f380b4f0ebfa7f37330b69ce8d4';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
 
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
     }
-    async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-    
-        const profile = await profileResponse.json();
-    
-        return {
-          profile
-        }
-      }
+  }
 }
